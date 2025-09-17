79a4e4de3695bf67d135a998b8e88c2f8cbba8ba
DevLuizFelip <luicostasantana@outlook.com>
2025-09-17 20:31:37 -0300

    ajsutes finais

--- prisma-site/src/components/Header.jsx
+++ prisma-site/src/components/Header.jsx
@@ -1,13 +1,15 @@
+// src/components/Header.jsx
+
 import { Link } from 'react-router-dom';
 
 function Header() {
   return (
     <header className="header">
       <Link to="/" className="logo">
-      <img src="/public/rsc-logo.png" alt="RSC & Engenharia Logo" className="logo-img" />
+        <img src="/rsc-logo.png" alt="RSC & Engenharia Logo" className="logo-img" />
       </Link>
       <nav className="nav">
-        <Link to="/">Home</Link> 
+        <Link to="/">Home</Link>
         <Link to="/sobre">O Escritório</Link>
         <Link to="/servicos">Serviços</Link>
         <Link to="/projetos">Projetos</Link>
